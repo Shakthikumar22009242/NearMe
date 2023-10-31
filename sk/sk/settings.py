d60ffd5aa5c685ceb3c9039498e9d5dfb39c9bbb
Shakthikumar1 <sgs14112004@gmail.com>
2023-10-31 22:40:26 +0530

    Success

--- sk/sk/settings.py
+++ sk/sk/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-4p&j0k1w8c6e%&!@tq^w7)bycc6=b06u8w*e8d#!iy3)_=#5ds
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -115,6 +116,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+STATICFILES_DIRS=[os.path.join(BASE_DIR,'mapapp/static']
 STATIC_URL = 'static/'
 
 # Default primary key field type
